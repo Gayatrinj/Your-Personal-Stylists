989f6359ccf22606af88ad31ad3d98b2eb6d9261
Gayatrinj <gayatrijawharkar2610@gmail.com>
2025-10-31 12:18:55 -0500

    updated app.tsx directly pushed to the main

--- personal-stylist/src/App.tsx
+++ personal-stylist/src/App.tsx
@@ -1,4 +1,15 @@
-import { StylistPage } from "./PersonalStylistUI";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import StylistPage from "./PersonalStylistUI/StylistPage";
+import SavedList from "./PersonalStylistUI/SavedList";
+
 export default function App() {
-  return <StylistPage />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<StylistPage />} />
+        <Route path="/saved" element={<SavedList />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
